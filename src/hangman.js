b150320558b6da74edc6a1f2a5aefeb3a61a02e1
Kevin <kbmp82@gmail.com>
2019-12-03 11:44:48 -0700

    updated code to be more readable

--- src/hangman.js
+++ src/hangman.js
@@ -1,6 +1,6 @@
 //hangman class
-class Hangman{
-  constructor(word, attempts){
+class Hangman {
+  constructor(word, attempts) {
     this.word = word
     this.wordLetter = word.toLowerCase().split('')
     this.attempts = attempts
@@ -9,68 +9,58 @@ class Hangman{
     this.won = false
   }
   //display result
-  displayResult(){
+  displayResult() {
     let answer = ''
-  this.wordLetter.map((letter) => {
-    const index = this.guessed.indexOf(letter)
-    if(index > -1){
-      answer += this.guessed[index]
-    }else if (letter === ' '){
+    this.wordLetter.map((letter) => {
+      const index = this.guessed.indexOf(letter)
+      if (index > -1) {
+        answer += this.guessed[index]
+      } else if (letter === ' ') {
         answer += ' &nbsp;'
-    }else{
-      answer += '_ '
-    }
-  })
+      } else {
+        answer += '_ '
+      }
+    })
 
-  //check game over
-  if(!this.gameOver && this.guessed.length > 0){
+    //check game over
+    if (!this.gameOver && this.guessed.length > 0) {
       document.querySelector('#message').innerHTML = this.status
-  // if(this.attempts > 0){
-  //
-  //   if(this.wordLetter.every((letter) => this.guessed.includes(letter))){
-  //       document.querySelector('#message').innerHTML = `Congrats! You won!`
-  //   }else{
-  //     document.querySelector('#message').innerHTML = `you have <strong>${this.attempts}</strong> attemps remaining...`
-  //   }
-  //
-  // }else{
-  //     this.gameOver = true
-  //   document.querySelector('#message').innerHTML = `GAME OVER, the word was ${this.word}`
-  // }
-}
-    document.querySelector('#word').innerHTML = '<h2>'+answer+'</h2>'
-}
+    }
+    document.querySelector('#word').innerHTML = '<h2>' + answer + '</h2>'
+  }
 
   //commit guess
-  makeGuess(letter){
-    if(!this.guessed.includes(letter)){
+  makeGuess(letter) {
+    if (!this.guessed.includes(letter)) {
       this.guessed.push(letter)
-      if(!this.wordLetter.includes(letter)) this.attempts -= 1
+      if (!this.wordLetter.includes(letter)) this.attempts -= 1
       this.displayResult()
-    }else{
+    } else {
       document.querySelector('#message').innerHTML = '<p>You have already guessed that letter</p>'
     }
   }
 
   //get status message
-  get status(){
+  get status() {
     let message = ''
-     if(this.attempts > 0){
-       if(this.wordLetter.every((letter) => this.guessed.includes(letter) || letter === ' ')){
-           return `Congrats! You won!`
-       }else{
-         return `you have <strong>${this.attempts}</strong> attemps remaining...`
-       }
+    if (this.attempts > 0) {
+      if (this.wordLetter.every((letter) => this.guessed.includes(letter) || letter === ' ')) {
+        return `Congrats! You won!`
+      } else {
+        return `you have <strong>${this.attempts}</strong> attemps remaining...`
+      }
 
-     }else{
-         this.gameOver = true
+    } else {
+      this.gameOver = true
       return `GAME OVER, the word was ${this.word}`
-     }
+    }
   }
 }
 
-export {Hangman as default}
-  //
+export { Hangman as default }
+
+/* Old code below that used constructors and prototypes - has been replaced by classes */
+
   // //contructor for Hangman
   // const TheGame = function(word, attempts){
   // this.word = word
--- src/requests.js
+++ src/requests.js
@@ -32,80 +32,14 @@
 //   })
 // }
 
-//using async await
-//using Fetch which has built in promises
+//using async await with fetch to simplify code
 const getPuzzle = async (wordCount) => {
-  const res = await fetch('https://puzzle.mead.io/puzzle?wordCount='+wordCount, {})
+  const res = await fetch('https://puzzle.mead.io/puzzle?wordCount=' + wordCount, {})
 
-  if(res.status === 200){
+  if (res.status === 200) {
     const data = await res.json()
-      return data.puzzle
-    }else{
-      throw new Error('unable to fetch the puzzle')
-    }
-}
-
-//get country
-const getCountry = async (countryCode) => {
-  const res = await fetch('https://restcountries.eu/rest/v2/all',{})
-  if(res.status === 200){
-    const data = await res.json()
-    const country = data.find(countryInfo => {
-      return countryInfo.alpha2Code === countryCode
-    })
-    return country.name
-  }else{
-    throw new Error('Could not fetch the country')
-  }
-
-  //   const request = new XMLHttpRequest()
-  // request.addEventListener('readystatechange', (e) => {
-  //   if(e.target.readyState === 4 && e.target.status === 200){
-  //     const response = JSON.parse(e.target.responseText)
-  //     const country = response.find(countryInfo => {
-  //       return countryInfo.alpha2Code === countryCode
-  //     })
-  //     callback(country.name)
-  //   }
-  //
-  // })
-  // request.open('GET', 'https://restcountries.eu/rest/v2/all')
-  // request.send()
-}
-
-
-//get location as promise
-
-// const getLocation = () => {
-//   return fetch('https://ipinfo.io?token=9471db2dfe3e41',{})
-//   .then((res) => {
-//     //console.log(res)
-//     if(res.status === 200){
-//     return res.json()
-//   }else{
-//     throw new Error('something went wrong')
-//   }
-//   })
-//   .then((data) =>{
-//     return getCountry(data.country)
-//     .then((res) => {
-//       data.fullCountry = res
-//       return data
-//     })
-//
-//   })
-// }
-
-//get location using async await
-const getLocation = async () => {
-  const res = await fetch('https://ipinfo.io?token=9471db2dfe3e41',{})
-  if(res.status === 200){
-  const data = await res.json()
-  data.fullCountry = await getCountry(data.country)
-  return data
-  }else{
-    throw new Error('something went wrong')
+    return data.puzzle
+  } else {
+    throw new Error('unable to fetch the puzzle')
   }
 }
-
-export {getPuzzle as default}
